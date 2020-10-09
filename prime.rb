1c9a89c7be26d14c825fffd14883685016ecc54e
AstralSol <porphyrios33@gmail.com>
2020-10-09 16:56:59 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+    start = 2
+    if number > 1
+        range = (start..number-1).to_a
+        range.none? do |pnum|
+        number % pnum == 0
+     end
+        else false
+    end
+end
